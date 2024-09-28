fb0f0841db25f6b8969033e85b54dd6ad7a673f8
Siqi Li <lisiqi9347@gmail.com>
2024-09-28 15:46:08 +0200

    add decorator

---
+++ decorator/0_decorator.py
@@ -0,0 +1,14 @@
+def mydecorator(function):
+    def wrapper(*args, **kwargs):
+        return_value = function(*args, **kwargs)
+        print("I am decorating your function")
+        return return_value
+    
+    return wrapper
+
+@mydecorator
+def hello(person):
+    print(f"Hello {person}!")
+    return f"Hello {person}!"
+
+print(hello("Mike"))
---
+++ decorator/2_timing.py
@@ -0,0 +1,20 @@
+# Practical Example of Decorator #2 - Timing
+
+import time
+
+def timed(function):
+    def wrapper(*args, **kwargs):
+        before = time.time()
+        value = function(*args, **kwargs)
+        after = time.time()
+        fname = function.__name__
+        print(f"{fname} took {after - before} seconds to execute!")
+        return value
+    return wrapper
+
+@timed
+def add(x, y):
+    time.sleep(2)
+    return x+y
+
+print(add(1, 2))
